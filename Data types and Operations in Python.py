d5f321db00ec640b388794f4ac7cbae175948ed8
AbdullahKashif5777 <155015769+AbdullahKashif5777@users.noreply.github.com>
2024-09-10 20:55:46 +0500

    Add files via upload

---
+++ Data types and Operations in Python.py
@@ -0,0 +1,89 @@
+#-------------------------------------------#
+#First Program to check how to print output #
+#-------------------------------------------#
+
+# print("Hello World!")
+# print("Hello\nWorld!")
+# print("Hello","World!")
+# print("Hello""World!")
+
+#-------------------------------------------#
+# Input in Python # 
+#-------------------------------------------#
+
+# str=input(("Enter your name:") )
+# print(str)
+
+
+
+
+
+#-------------------------------------------#
+# Operations in Python # 
+#-------------------------------------------#
+
+# => Arithmatic Operations 
+
+# a=2
+# b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b) #( Always return answer in float)
+# print(a%b) #( Always return remainder)
+# print(a**b)#( Always used for  power of a number)
+
+# => Assignment Operations 
+
+# a=2
+# b=2
+# a+=b
+# a-=b
+# a*=b
+# a/=b
+# a%=b
+# a**=b
+# print(a,"\n",b)
+
+# => Relational / Comperision Operations
+
+# a=2
+# b=3
+# print(a==b)
+# print(a!=b)
+# print(a<=b)
+# print(a>=b)
+# print(a<b)
+# print(a>b)
+
+# => Logical Operations
+
+# a=2
+# b=6
+# print(not(a==b))
+# print(not(a!=b))
+# print(not(a!=b) or (a!=b))
+# print(not(a!=b) and (a!=b))
+# print(a or b)
+# print(a and b)
+
+#-------------------------------------------#
+# Converstions in Python # 
+#-------------------------------------------#
+
+# # => Type conversion (Automatically by compiler)
+# a=2
+# b=4.5
+# sum=a+b
+# print(type(sum))
+# print(sum)
+
+# # => Type casting (Manually by programer)
+
+# a="4"
+# b=2.2
+# c=int(a)
+# print(b+c)
+
+
+
