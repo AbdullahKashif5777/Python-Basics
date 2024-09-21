49f121b3cb9f8e3a42db77923bd73b174e88988c
AbdullahKashif5777 <155015769+AbdullahKashif5777@users.noreply.github.com>
2024-09-21 18:03:03 +0500

    Add files via upload

---
+++ Functions & Recursions in Python.py
@@ -0,0 +1,58 @@
+#--------------------------------=-#
+# Functions & Recursions in Python
+#----------------------------------#
+
+# => Functions in PYthon <= #
+
+# def calculateSum(a,b):
+#     sum=a+b
+#     print(sum)
+#     return sum
+# calculateSum(4,5)
+
+# names=["ali","ahmad","waleed","hammad"]
+# def PrintLen(list):
+#     print(len(list))
+# PrintLen(names)
+
+# sections=["W1","W2","W3","W4","W5"]
+# def Print_In_Line(list):
+#     i=0
+#     while(i<len(list)):
+#         print(list[i],end=" ")
+#         i+=1
+# Print_In_Line(sections)
+
+# def factorial(n):
+#     fac=1
+#     for i in range(1,n+1):
+#         fac*=i
+#     print(fac)
+# factorial(33)
+
+# => Recursion in Python <= #
+
+
+# def show(n):
+#     if(n==0):
+#         return
+#     print(n)
+#     show(n-1)
+# show(7)
+
+# def factorial(n):
+#     if(n==0 or n==1):
+#         return 1
+#     fac=n*factorial(n-1)
+#     return fac
+# print(factorial(5))
+
+# def cal_Sum(n):
+#     if(n==0):
+#         return 0
+#     return cal_Sum(n-1)+n
+# sum=(cal_Sum(2))
+# print(sum)
+
+
+
