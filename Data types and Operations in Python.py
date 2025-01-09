068f636f7ab39dd56877cbac284feb32c026047a
AbdullahKashif5777 <155015769+AbdullahKashif5777@users.noreply.github.com>
2025-01-10 01:46:11 +0500

    Update Data types and Operations in Python.py

--- Data types and Operations in Python.py
+++ Data types and Operations in Python.py
@@ -2,22 +2,22 @@
 #First Program to check how to print output #
 #-------------------------------------------#
 
-# print("Hello World!")
-# print("Hello\nWorld!")
-# print("Hello","World!")
-# print("Hello""World!")
+print("Hello World!")
+print("Hello\nWorld!")
+print("Hello","World!")
+print("Hello""World!")
 
 #-------------------------------------------#
 # Input in Python # 
 #-------------------------------------------#
 
-# age=input(("Enter your age:") )
-# print(type(age))
-# print(age)
+age=input(("Enter your age:") )
+print(type(age))
+print(age)
 
-# agee=int(input(("Enter your age:")))
-# print(type(agee))
-# print(agee)
+agee=int(input(("Enter your age:")))
+print(type(agee))
+print(agee)
 
 #-------------------------------------------#
 # Operations in Python # 
@@ -25,66 +25,66 @@
 
 # => Arithmatic Operations 
 
-# a=2
-# b=5
-# print(a+b)
-# print(a-b)
-# print(a*b)
-# print(a/b) #( Always return answer in float)
-# print(a%b) #( Always return remainder)
-# print(a**b)#( Always used for  power of a number)
+a1=2
+b1=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) #( Always return answer in float)
+print(a%b) #( Always return remainder)
+print(a**b)#( Always used for  power of a number)
 
 # => Assignment Operations 
 
-# a=2
-# b=2
-# a+=b
-# a-=b
-# a*=b
-# a/=b
-# a%=b
-# a**=b
-# print(a,"\n",b)
+a2=2
+b2=2
+a2+=b2
+a2-=b2
+a2*=b2
+a2/=b2
+a2%=b2
+a2**=b2
+print(a,"\n",b)
 
 # => Relational / Comperision Operations
 
-# a=2
-# b=3
-# print(a==b)
-# print(a!=b)
-# print(a<=b)
-# print(a>=b)
-# print(a<b)
-# print(a>b)
+a3=2
+b3=3
+print(a3==b3)
+print(a3!=b3)
+print(a3<=b3)
+print(a3>=b3)
+print(a3<b3)
+print(a3>b3)
 
 # => Logical Operations
 
-# a=2
-# b=6
-# print(not(a==b))
-# print(not(a!=b))
-# print(not(a!=b) or (a!=b))
-# print(not(a!=b) and (a!=b))
-# print(a or b)
-# print(a and b)
+a4=2
+b4=6
+print(not(a4==b4))
+print(not(a4!=b4))
+print(not(a4!=b4) or (a4!=b4))
+print(not(a4!=b4) and (a4!=b4))
+print(a4 or b4)
+print(a4 and b4)
 
 #-------------------------------------------#
 # Converstions in Python # 
 #-------------------------------------------#
 
 # # => Type conversion (Automatically by compiler)
-# a=2
-# b=4.5
-# sum=a+b
-# print(type(sum))
-# print(sum)
+a5=2
+b5=4.5
+sum=a5+b5
+print(type(sum))
+print(sum)
 
 # # => Type casting (Manually by programer)
 
-# a="4"
-# b=2.2
-# c=int(a)
-# print(b+c)
+a6="4"
+b6=2.2
+c=int(a6)
+print(b6+c)
 
 
 
